a07d5f1b808c37f5fe4bcff28f75283a7af36821
Jorge Bonilla <JorgeB27@gmail.com>
2019-11-22 22:42:37 -0800

    updates to the code for supervised learning on 11.22.19

--- Supervised_Learning_11_22_19.py
+++ Supervised_Learning_11_22_19.py
@@ -1,3 +1,31 @@
 # Jorge Bonilla
 
+# Packages
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+from scipy import sparse
+import matplotlib.pyplot as plt
+import mglearn
 # Chapter 2 - Supervised Learning
+
+# Supervised learning is used whenever we want to predict a certain outcome from a given
+# input and we have examples of input/output pairs.
+
+# The goal is to make accurate predictions for new, never-before-seen data.
+
+# Supervised Machine Learning Algorithms
+
+# Generate Dataset
+
+X, y = mglearn.datasets.make_forge()
+
+# Plot Dataset
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.legend(["Class 0", "Class 1"], loc = 4)
+plt.xlabel("First Feature")
+plt.ylabel("Second Feature")
+print("X.shape: {}".format(X.shape))
+plt.show()
