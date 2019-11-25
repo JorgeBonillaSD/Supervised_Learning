f24dcc3a8da51fccf6cdac09029692e3534da96d
Jorge Bonilla <JorgeB27@gmail.com>
2019-11-24 19:44:38 -0800

    updates to the code for supervised learning on 11.22.19

--- Supervised_Learning_11_22_19.py
+++ Supervised_Learning_11_22_19.py
@@ -29,3 +29,55 @@ plt.xlabel("First Feature")
 plt.ylabel("Second Feature")
 print("X.shape: {}".format(X.shape))
 plt.show()
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X, y, 'o')
+plt.ylim(-3, 3)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+
+
+# ------- Breast Cancer Dataset ----------
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer = load_breast_cancer()
+print("cancer.keys(): \n{}".format(cancer.keys()))
+print("Shape of Cancer data: {}".format(cancer.data.shape))
+print("Sample counts per class:\n{}".format(
+    {n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}
+))
+
+print("Feature names: \n{}".format(cancer.feature_names))
+
+X, y = mglearn.datasets.load_extended_boston()
+print("X.shape: {}".format(X.shape))
+
+# ------- k-Nearest Neighbors ----------
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+# Split the data into a training set and a test set.
+X, y = mglearn.datasets.make_forge()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+clf = KNeighborsClassifier(n_neighbors = 3)
+clf.fit(X_train, y_train)
+print("Test set predictions: {}".format(clf.predict(X_test)))
+print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
+
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+
+for n_neighbors, ax in zip ([1, 3, 9], axes):
+    # the fit method returns the object self, so we can instantiate and fit in one line.
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill = True, eps = 0.5, ax=ax, alpha=0.4)
+    mglearn.discrete_scatter(X[:,0], X[:,1], y, ax=ax)
+    ax.set_title("{} neighbor(s)".format(n_neighbors))
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+axes[0].legend(loc = 3)
